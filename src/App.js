7576a2147a12f141914fc9951fa605bfbebc11c5
mahsa enghelabi <mhsk642@gmail.com>
2022-06-17 14:53:53 +0430

    display flex App

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React from 'react'
 import headImage from "./Components/Vector1.png"
 import nemodarImage from  "./Components/Rectangle 9.png"
 import tableImage from "./Components/Rectangle 10.png"
-// 
+import listImage from "./Components/Group 19.png"
+import leftSide from "./Components/Rectangle 2.png"
+import irancellImage from './Components/irancell.png'
+import "./App.css"
+
 function App() {
   return (
-    <div>
+    <div className='App'>
+
+<div className='right'>
+
+<img src={irancellImage}/>
+<input type="text" placeholder='جستجو'/>
+<img src={listImage}/>
+</div>
 
 
+<div className='center'>
+<h3>صفحه اصلی</h3>
       <img src={headImage}/>
       <img src={nemodarImage}/>
       <img src={tableImage}/>
+</div>
+
+
+<div className='left'>
+<img src={leftSide}/>
+</div>
+  
     </div>
   )
 }
